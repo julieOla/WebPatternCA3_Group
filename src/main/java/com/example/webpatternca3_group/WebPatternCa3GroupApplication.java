87216cd8d6f8a1fafa46c60ae4e2de98b365fe6e
JulieOlamijuwon <d00215779@student.dkit.ie>
2024-12-18 13:01:13 +0000

    Committing: Project creation

---
+++ src/main/java/com/example/webpatternca3_group/WebPatternCa3GroupApplication.java
@@ -0,0 +1,13 @@
+package com.example.webpatternca3_group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPatternCa3GroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebPatternCa3GroupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webpatternca3_group/WebPatternCa3GroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webpatternca3_group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPatternCa3GroupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
